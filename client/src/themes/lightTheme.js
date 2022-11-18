575d7df4096841f093c138396d1bfa5a4e8efb5c
Reqvel <sergey.sh.hodakovs@gmail.com>
2022-11-18 23:17:45 +0300

    theme extended

--- client/src/themes/lightTheme.js
+++ client/src/themes/lightTheme.js
@@ -2,8 +2,10 @@ import mainTheme from "./mainTheme";
 
 const lightTheme = {
     textPrimary: '#25282E',
-    textSecondary: '#989898',
+    textSecondary: '#FFFFFF',
+    textSubtile: '#989898',
     textDisabled: '#CCCCCC',
+    buttonColor: '#25282E',
     buttonColorHover: '#F4F4F4',
     buttonBorderColor: '#989898',
     mainTheme: mainTheme,
