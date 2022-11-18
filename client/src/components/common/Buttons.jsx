95c2b3bfcecf944da6f6020be9f904f8677f50f3
Reqvel <sergey.sh.hodakovs@gmail.com>
2022-11-18 22:44:38 +0300

    button improved

--- client/src/components/common/Buttons.jsx
+++ client/src/components/common/Buttons.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.button`
-    padding: 12px 48px 12px;
+    padding: ${props => props.padding ? props.padding : '12px 48px'};
     font-size: ${props => props.fontSize};
     font-weight: 600;
     color: ${props => props.textColor};
-    background: ${props => props.buttonColor};
+    background: ${props => props.buttonColor ? props.buttonColor : 'transparent'};
     border-radius: 12px;
-    border: 2px solid ${props => props.borderColor};
+    border: 2px solid ${props => props.borderColor ? props.borderColor : 'transparent'};
     cursor: pointer;
     transition: background 0.3s ease; 
 
@@ -18,14 +18,16 @@ const Container = styled.button`
     };
 `;
 
-export const Button = ({fontSize, 
+export const Button = ({padding,
+                        fontSize, 
                         textColor, 
                         buttonColor, 
                         buttonColorHover, 
                         borderColor, 
                         text}) => {
   return (
-    <Container fontSize={fontSize}
+    <Container padding={padding}
+               fontSize={fontSize}
                textColor={textColor}
                buttonColor={buttonColor}
                buttonColorHover={buttonColorHover}
