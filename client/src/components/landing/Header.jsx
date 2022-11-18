9676df6b40543eae8b38a36425f600e9ce0e5a31
Reqvel <sergey.sh.hodakovs@gmail.com>
2022-11-18 17:04:40 +0300

    Landing page header

--- client/src/components/landing/Header.jsx
+++ client/src/components/landing/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components'
 import LogoText from '../common/LogoText';
 import NavBar from './NavBar';
+import { navBarItems } from '../../data/data';
+import { Button } from '../common/Buttons';
 
 const Container = styled.header`
   padding-top: 24px;
@@ -18,27 +20,43 @@ const InnerContainer = styled.div`
 `;
 
 const InnerContainerComponent = styled.div`
+  display: flex;
+  justify-content: ${props => props.justifyContent};
   flex-grow: 1;
   flex-basis: 0;
+  padding: 16px;
+  gap: ${props => props.gap};
 `;
 
 const Header = () => {
   return (
     <Container>
       <InnerContainer>
-        <InnerContainerComponent>
+        <InnerContainerComponent justifyContent='start'>
           <LogoText fontSize='3.052rem' 
                     color={props => props.theme.textPrimary}
                     text='Mio'/>
         </InnerContainerComponent>
-        <InnerContainerComponent>
-          <NavBar textColor={props => props.theme.textSecondary}
+        <InnerContainerComponent justifyContent='center'>
+          <NavBar fontSize='0.8rem'
+                  textColor={props => props.theme.textSecondary}
                   textColorHover={props => props.theme.textPrimary}
                   underscoreColor={props => props.theme.mainTheme.accentColor}
-                  items={['Home', 'About Us', 'Pricing']}/>
+                  items={navBarItems}/>
         </InnerContainerComponent>
-        <InnerContainerComponent>
-          <a>hello</a>
+        <InnerContainerComponent justifyContent='end' gap='12px'>
+          <Button fontSize='1rem'
+                  textColor={props => props.theme.textPrimary}
+                  buttonColor='transparent'
+                  buttonColorHover={props => props.theme.buttonColorHover}
+                  borderColor='transparent'
+                  text='Sign In'/>
+          <Button fontSize='1rem'
+                  textColor={props => props.theme.textPrimary}
+                  buttonColor='transparent'
+                  buttonColorHover={props => props.theme.buttonColorHover}
+                  borderColor={props => props.theme.buttonBorderColor}
+                  text='Sign Up'/>
         </InnerContainerComponent>
       </InnerContainer>
     </Container>
