2b7d430e13f558dab68f566fa1ef7f449daac2a6
Reqvel <sergey.sh.hodakovs@gmail.com>
2022-11-24 23:02:30 +0300

    assets

---
+++ client/src/data/data.jsx
@@ -0,0 +1,21 @@
+import { ReactComponent as CalendarIcon } from '../svgs/CalendarIcon.svg';
+
+export const logoText = 'Mio';
+export const navBarItems = ['Home', 'About Us', 'Pricing'];
+export const aboutUsCardsInfo = [
+    {
+        icon: <CalendarIcon/>,
+        header: 'Calendar',
+        details: 'Illuminate business-critical insights by tapping into the worlds largest and most transparent focus group: social media.'
+    },
+    {
+        icon: <CalendarIcon/>,
+        header: 'Calendar',
+        details: 'Illuminate business-critical insights by tapping into the worlds largest and most transparent focus group: social media.'
+    },
+    {
+        icon: <CalendarIcon/>,
+        header: 'Calendar',
+        details: 'Illuminate business-critical insights by tapping into the worlds largest and most transparent focus group: social media.'
+    },
+];
