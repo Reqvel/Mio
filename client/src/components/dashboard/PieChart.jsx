c921209d1f3a8ea31bfb5d77c335193ef5c85f9b
Reqvel <sergey.sh.hodakovs@gmail.com>
2022-12-07 22:55:52 +0300

    pie chart Template

---
+++ client/src/components/dashboard/PieChart.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { PieChart as PieChartRecharts, 
+         Pie, 
+         Cell,
+         Tooltip, 
+         ResponsiveContainer } from 'recharts';
+
+const PieChart = () => {
+  const data = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+    { name: 'Group D', value: 200 },
+  ];
+  
+  const COLORS = ['red', '#00C49F', '#FFBB28', '#FF8042'];
+
+  return (
+    <ResponsiveContainer>
+      <PieChartRecharts>
+        <Tooltip/>
+        <Pie
+          data={data}
+          outerRadius={100}
+          innerRadius={65}
+          fill="#8884d8"
+          dataKey="value"
+          label
+          paddingAngle={2}
+        >
+          {data.map((entry, index) => (
+            <Cell key={index} fill={COLORS[index]}/>
+          ))}
+        </Pie>
+      </PieChartRecharts>
+    </ResponsiveContainer>
+  )
+}
+
+export default PieChart
