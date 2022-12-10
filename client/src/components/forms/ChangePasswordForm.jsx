a8c82f7eb7b27ce763798b553ddc6fc4758ef491
Reqvel <sergey.sh.hodakovs@gmail.com>
2022-12-10 18:48:26 +0300

    change password form

--- client/src/components/forms/ChangePasswordForm.jsx
+++ client/src/components/forms/ChangePasswordForm.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import FormField from './FormField'
 import PasswordIcon from '../../svgs/icons/Lock.svg'
 import { ButtonBig } from '../common/Buttons'
+import { getResetTokensFromUrl } from '../../utils/GetResetTokensFromUrl'
+import { useSetNewPasswordMutation } from '../../redux/features/authApiSlice'
+import { useNavigate } from 'react-router-dom'
+import pagesPaths from '../../routes/PagesPaths'
 
-const Container = styled.div`
+const Container = styled.form`
   display: flex;
   flex-direction: column;
 `
@@ -28,23 +32,94 @@ const Fields = styled.div`
   margin-bottom: 56px;
 `
 
+const ErrMsg = styled.span`
+  display: ${props => props.isVisible ? 'inline' : 'none'};
+  color: red;
+  font-weight: 600;
+  margin-bottom: 24px;
+`
+
 const ChangePasswordForm = () => {
+  const pwdRef = useRef()
+  const pwdConfirmRef = useRef()
+  const [pwd, setPwd] = useState('')
+  const [pwdConfirm, setPwdConfirm] = useState('')
+  const [token, setToken] = useState(null)
+  const [uidb64, setUidb64] = useState(null)
+  const [errMsg, setErrMsg] = useState('')
+  const navigate = useNavigate()
+  const [setNewPassword, { isLoading }] = useSetNewPasswordMutation()
+
+  const handlePwdInput = (e) => setPwd(e.target.value)
+  const handlePwdConfirmInput = (e) => setPwdConfirm(e.target.value)
+
+  useEffect(() => {
+    const { token, uidb64 } = getResetTokensFromUrl(window.location.search)
+    setToken(token)
+    setUidb64(uidb64)
+    pwdRef.current.focus()
+  }, [])
+
+  useEffect(() => {
+    setErrMsg('')
+  }, [pwd, pwdConfirm])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if(pwd === pwdConfirm) {
+      try {
+        const response = await setNewPassword({ password: pwd,
+                                                token,
+                                                uidb64 }).unwrap()
+        setPwd('')
+        setPwdConfirm('')
+        navigate(pagesPaths.passwordChanged)
+      } catch (err) {
+        pwdRef.current.focus()
+        setErrMsg('Error')
+      }
+    } else {
+      pwdRef.current.focus()
+      setErrMsg("Passwords doesn't match")
+    }
+  }
+
   return (
-    <Container>
+    <Container onSubmit={handleSubmit}>
       <Header>Change password</Header>
       <Details>
         Enter your user account's new password and confirm it.
       </Details>
       <Fields>
         <FormField label='New password' 
-                   placeholder='Enter new password'
-                   icon={PasswordIcon}/>
+                    icon={PasswordIcon}
+                    placeholder='Enter new password'
+                    refTo={pwdRef}
+                    value={pwd}
+                    onChange={handlePwdInput}
+                    disabled={isLoading}
+                    type={'password'}
+                    pattern={".{8,}"}
+                    title={"8 characters minimum"}/>
         <FormField label='Confirm new password' 
-                   placeholder='Enter new password'
-                   icon={PasswordIcon}/>
+                    icon={PasswordIcon}
+                    placeholder='Enter new password'
+                    refTo={pwdConfirmRef}
+                    value={pwdConfirm}
+                    onChange={handlePwdConfirmInput}
+                    disabled={isLoading}
+                    type={'password'}
+                    pattern={".{8,}"}
+                    title={"8 characters minimum"}/>
       </Fields>
-      <ButtonBig>
-        Change password
+      <ErrMsg isVisible={errMsg}>
+        {errMsg}
+      </ErrMsg>
+      <ButtonBig disabled={isLoading}>
+        {!isLoading
+          ? 'Change password'
+          : 'Sending...'}
       </ButtonBig>
     </Container>
   )
