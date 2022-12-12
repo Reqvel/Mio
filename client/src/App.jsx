4096d262b08fa8e67a6c1752a7257f198b6ab842
Reqvel <sergey.sh.hodakovs@gmail.com>
2022-12-12 19:35:51 +0300

    app theme support

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
-import lightTheme from './themes/lightTheme';
+import lightTheme from './themes/LightTheme';
+import darkTheme from './themes/DarkTheme';
 import AppRoutes from './routes/AppRoutes';
+import { useSelector, useDispatch } from 'react-redux';
+import { isThemeDarkKey } from './redux/features/appSlice';
+import { setIsThemeDark } from './redux/features/appSlice';
 
 const App = () => {
+  const [theme, setTheme] = useState(darkTheme)
+  const { isThemeDark } = useSelector(state => state.app)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (isThemeDarkKey in localStorage) {
+      const val = (localStorage.getItem(isThemeDarkKey) === 'true')
+      dispatch(setIsThemeDark(val))
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isThemeDark) {
+      setTheme(darkTheme)
+    } else {
+      setTheme(lightTheme)
+    }
+  }, [isThemeDark])
+
   return (
     <>
-      <GlobalStyle/>
-      <BrowserRouter>
-        <ThemeProvider theme={lightTheme}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle/>
+        <BrowserRouter>
           <AppRoutes/>
-        </ThemeProvider>
-      </BrowserRouter>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   )
 };
