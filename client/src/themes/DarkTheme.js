7a6cd8046affb4ddd54d5e192aee9d04111c24cc
Reqvel <sergey.sh.hodakovs@gmail.com>
2022-12-12 19:35:13 +0300

    dark theme init

---
+++ client/src/themes/DarkTheme.js
@@ -0,0 +1,106 @@
+const darkTheme = {
+    color: {
+      accent: '#FEB062'
+    },
+    backgroundColor: {
+      primary: '#222427',
+      secondary: '#25282E',
+    },
+    maxWidth: {
+      landing: '1280px',
+      dashboard: '2000px',
+      signIn: '1280px',
+      signUp: '1280px',
+    },
+    padding: {
+      landing: '16px',
+      signIn: '16px',
+      signUp: '16px',
+      dashboard: '64px',
+    },
+    textColor: {
+      primary: '#DBDBDB',
+      secondary: '#222427',
+      subtile: '#707070',
+    },
+    gap: {
+      cards: {
+        landing: '24px',
+        top: '24px',
+        dashboard: '24px',
+      },
+    },
+    components: {
+      footer: {
+        color: '#DBDBDB'
+      },
+      button: {
+        filled: {
+          color: '#DBDBDB',
+          disabledColor: '#989898',
+          hoverColor: '#DBDBDB',
+        },
+        transparent: {
+          hoverColor: '#353535',
+        },
+        outlined: {
+          borderColor: '#989898',
+        },
+      },
+      card: {
+        featureColor: '#DBDBDB',
+        pricing: {
+          borderColor: '#CCCCCC',
+        },
+        topColor: '#25282E',
+        dashboard: {
+          borderColor: '#CCCCCC',
+          color: '#FFFFFF',
+        }
+      },
+      inputField: {
+        borderColor: '#989898',
+        outlineColor: '#25282E',
+      },
+      percentage: {
+        upColor: '#0CAF60',
+        downColor: '#FA5F1C',
+      },
+      calendar: {
+        borderColor: '#989898',
+        headerColor: '#25282E',
+        top: {
+          color: '#FFFFFF',
+          borderColor: '#CCCCCC',
+        },
+        side: {
+          color: '#FFFFFF',
+          borderColor: '#CCCCCC',
+        },
+        cell: {
+          color: '#FAFAFA',
+          subtileColor: '#F4F4F4',
+          borderColor: '#D2D2D2',
+        },
+        card: {
+          color: '#25282E',
+          subtileColor: '#F4F4F4',
+          borderColor: '#D2D2D2',
+        },
+      },
+      kanban: {
+        card: {
+          color: '#FFFFFF',
+          headerColor: '#25282E',
+          borderColor: '#CCCCCC',
+          cellColor: '#FAFAFA',
+        },
+      },
+      spinner: {
+        backgroundColor: '#CCCCCC',
+        foregroundColor: '#25282E',
+      },
+    }
+  };
+  
+  export default darkTheme;
