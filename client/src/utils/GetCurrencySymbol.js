040efbe7e241a2738c7fccd5601c37b618aee647
Reqvel <sergey.sh.hodakovs@gmail.com>
2022-12-14 21:58:00 +0300

    get currency symbol

---
+++ client/src/utils/GetCurrencySymbol.js
@@ -0,0 +1,25 @@
+const currencySymbols = {
+    'USD': '$', // US Dollar
+    'EUR': '€', // Euro
+    'CRC': '₡', // Costa Rican Colón
+    'GBP': '£', // British Pound Sterling
+    'ILS': '₪', // Israeli New Sheqel
+    'INR': '₹', // Indian Rupee
+    'JPY': '¥', // Japanese Yen
+    'KRW': '₩', // South Korean Won
+    'NGN': '₦', // Nigerian Naira
+    'PHP': '₱', // Philippine Peso
+    'PLN': 'zł', // Polish Zloty
+    'PYG': '₲', // Paraguayan Guarani
+    'THB': '฿', // Thai Baht
+    'UAH': '₴', // Ukrainian Hryvnia
+    'VND': '₫', // Vietnamese Dong
+};
+
+export function getCurrencySymbol(currencyName) {
+    currencyName = currencyName.toUpperCase()
+    if(currencySymbols[currencyName]!==undefined) {
+        return currencySymbols[currencyName]
+    }
+    return ''
+} 
