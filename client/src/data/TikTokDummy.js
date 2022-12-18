5ce80f5348ca924d3e366d3a9477d378dc30912f
Reqvel <sergey.sh.hodakovs@gmail.com>
2022-12-18 22:08:06 +0300

    tiktok data

---
+++ client/src/data/TikTokDummy.js
@@ -0,0 +1,195 @@
+const tikTokDummy = {
+    followersGrowth : [
+        {
+            date: '06.12.2022',
+            followers: 28344,
+        },
+        {
+            date: '07.12.2022',
+            followers: 35964,
+        },
+        {
+            date: '08.12.2022',
+            followers: 41600,
+        },
+        {
+            date: '09.12.2022',
+            followers: 25600,
+        },
+        {
+            date: '10.12.2022',
+            followers: 21370,
+        },
+        {
+            date: '11.12.2022',
+            followers: 20543,
+        },
+        {
+            date: '12.12.2022',
+            followers: 17943,
+        },
+        {
+            date: '13.12.2022',
+            followers: 23547,
+        },
+        {
+            date: '14.12.2022',
+            followers: 24577,
+        },
+        {
+            date: '15.12.2022',
+            followers: 25547,
+        },
+        {
+            date: '16.12.2022',
+            followers: 29343,
+        },
+        {
+            date: '17.12.2022',
+            followers: 84543,
+        },
+        {
+            date: '18.12.2022',
+            followers: 53565,
+        },
+        {
+            date: '19.12.2022',
+            followers: 45344,
+        },
+        {
+            date: '20.12.2022',
+            followers: 34345,
+        },
+    ],
+    likesGrowth: [
+        {
+            date: '06.12.2022',
+            likes: 329543,
+        },
+        {
+            date: '07.12.2022',
+            likes: 523543,
+        },
+        {
+            date: '08.12.2022',
+            likes: 659543,
+        },
+        {
+            date: '09.12.2022',
+            likes: 703543,
+        },
+        {
+            date: '10.12.2022',
+            likes: 402543,
+        },
+        {
+            date: '11.12.2022',
+            likes: 301543,
+        },
+        {
+            date: '12.12.2022',
+            likes: 459543,
+        },
+        {
+            date: '13.12.2022',
+            likes: 524543,
+        },
+        {
+            date: '14.12.2022',
+            likes: 650543,
+        },
+        {
+            date: '15.12.2022',
+            likes: 734543,
+        },
+        {
+            date: '16.12.2022',
+            likes: 860543,
+        },
+        {
+            date: '17.12.2022',
+            likes: 900543,
+        },
+        {
+            date: '18.12.2022',
+            likes: 1700543,
+        },
+        {
+            date: '19.12.2022',
+            likes: 506543,
+        },
+        {
+            date: '20.12.2022',
+            likes: 432543,
+        },
+    ],
+    videoGrowth: [
+        {
+            date: '06.12.2022',
+            videos: 0,
+        },
+        {
+            date: '07.12.2022',
+            videos: 0,
+        },
+        {
+            date: '08.12.2022',
+            videos: 1,
+        },
+        {
+            date: '09.12.2022',
+            videos: 2,
+        },
+        {
+            date: '10.12.2022',
+            videos: 0,
+        },
+        {
+            date: '11.12.2022',
+            videos: 2,
+        },
+        {
+            date: '12.12.2022',
+            videos: 0,
+        },
+        {
+            date: '13.12.2022',
+            videos: 0,
+        },
+        {
+            date: '14.12.2022',
+            videos: 0,
+        },
+        {
+            date: '15.12.2022',
+            videos: 1,
+        },
+        {
+            date: '16.12.2022',
+            videos: 0,
+        },
+        {
+            date: '17.12.2022',
+            videos: 1,
+        },
+        {
+            date: '18.12.2022',
+            videos: 3,
+        },
+        {
+            date: '19.12.2022',
+            videos: 0,
+        },
+        {
+            date: '20.12.2022',
+            videos: 0,
+        },
+    ],
+    engagement: [
+        { name: 'Likes %', value: 9.95 },
+        { name: 'Comments %', value: 0.17 },
+        { name: 'Shares %', value: 0.03 },
+    ],
+}
+
+export default tikTokDummy
