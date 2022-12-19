7bedfcaa805d25be4d1e2cddcd1ff3d0dee18d04
Reqvel <sergey.sh.hodakovs@gmail.com>
2022-12-19 05:47:16 +0300

    formatters adapted

--- client/src/utils/Formatters.js
+++ client/src/utils/Formatters.js
@@ -6,7 +6,18 @@ export function numFormatter(num) {
 }
 
 export function dateFormatter(date) {
-    const dateParts = date.split(".");
-    const dateObject = format(new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]), "dd MMM");
+    let dateParts = []
+    let dateObject
+    if (date.includes(".")) {
+        dateParts = date.split(".");
+    } else if (date.includes("-")) {
+        dateParts = date.split("-");
+    }
+
+    if (dateParts.length === 3) {
+        dateObject = format(new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]), "dd MMM");
+    } else if (dateParts.length === 2) {
+        dateObject = format(new Date(+dateParts[0], dateParts[1] - 1, +'01'), "dd MMM");
+    }
     return dateObject
 };
