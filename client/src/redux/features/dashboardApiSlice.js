796b180751bfa4986b28cbe71ea7919b677e623e
Reqvel <sergey.sh.hodakovs@gmail.com>
2022-12-19 05:47:57 +0300

    dashboard endpoints

--- client/src/redux/features/dashboardApiSlice.js
+++ client/src/redux/features/dashboardApiSlice.js
@@ -2,8 +2,25 @@ import { apiSlice } from "../services/apiSlice";
 
 export const dashboardApiSlice = apiSlice.injectEndpoints({
   endpoints: builder => ({
+
+    getYouTubeStats: builder.mutation({
+      query: payload => ({
+        url: '/statistics/youtube/user/',
+        method: 'POST',
+        body: { ...payload }
+      }),
+    }),
+
+    getUsersInfo: builder.query({
+      query: () => ({
+        url: '/account/user/',
+        method: 'GET',
+      }),
+    }),
     
   })
 })
 
-export const {} = dashboardApiSlice
+export const { useGetYouTubeStatsMutation,
+               useGetUsersInfoQuery,
+               useLazyGetUsersInfoQuery } = dashboardApiSlice
